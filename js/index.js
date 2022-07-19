3e8b2335ef554cd9be3d24405175658278414106
RaulFR01 <rferrazrobles@hotmail.com>
2022-07-19 19:49:25 +0200

    Iteration 1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 // Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+let hacker1 = "Rodolfo";
 
+console.log("The driver's name is " + hacker1);
+
+let hacker2 = "Matias";
+
+console.log("The navigator's name is " + hacker2);
+
+// Iteration 2: Conditionals
 
 // Iteration 3: Loops
